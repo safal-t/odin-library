dfedcda0f90ffc0f9f6140a6f97e420a121f6e5e
safal-t <safaltimalsena0@gmail.com>
2025-04-21 07:30:11 +1000

    feat: add function to display library array as 'card' divs on the website

---
+++ script.js
@@ -0,0 +1,32 @@
+const myLibrary = []
+
+function Book(title, author, publishedDate) {
+    this.title = title;
+    this.author = author;
+    this.readStatus = false;
+    this.publishedDate = publishedDate
+    this.id = crypto.randomUUID();
+}
+
+function addBookToMyLibrary(title, author, publishedDate) {
+    const newBook = new Book(title, author, publishedDate);
+    myLibrary.push(newBook); 
+} 
+
+function addLibaryToPage(array) {
+    for (const book of array) {
+            const newDiv = document.createElement("div");
+            newDiv.classList.add("book-card")
+            newDiv.innerText += book.title;
+            document.body.append(newDiv);
+    } 
+}
+
+
+// Adding some books to the library
+addBookToMyLibrary("The Great Gatsby", "F. Scott Fitzgerald", "1925");
+addBookToMyLibrary("To Kill a Mockingbird", "Harper Lee", "1960");
+addBookToMyLibrary("1984", "George Orwell", "1949");
+
+// Display the library
+addLibaryToPage(myLibrary);
